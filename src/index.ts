2546735acbad42da213990cb113fe0ea2d7fbaf7
Dacre <dacre@mooce.co.nz>
2022-03-12 11:48:50 +1300

    main app and rendering loop implemented

--- src/index.ts
+++ src/index.ts
@@ -1,288 +1,400 @@
-var m4 = require("gl-mat4");
-
-import { loadMdl } from "./mdl";
+import {
+  UiCanvas,
+  UiDropZone,
+  UiFilePicker,
+  UiRadioGroup,
+  UiSelect,
+  UiStatus,
+} from "./components";
+import { ArrayReader } from "./mdl/ArrayReader";
+import { fileFromPath } from "./files";
+import { loadMdl } from "./mdl/loader";
 import {
   bindBuffer,
   bindProgram,
   createAttributeBuffer,
-  createIndicesBuffer,
   createProgram,
   createTexture,
+  deleteBuffer,
+  deleteProgram,
+  deleteTexture,
 } from "./rendering";
+import SHADER_FS from "./shaders/mesh.fs";
+import SHADER_VS from "./shaders/mesh.vs";
+import { buildFrameDeltas, extractAnimationsFromFrames } from "./mdl/helpers";
+import { MdlAnimation } from "./mdl/types";
 
-const SHADER_VS = `
-#version 100
-precision mediump float;
+const m4 = require("gl-mat4");
 
-uniform float u_frac;
-uniform mat4 u_viewProjection;
-uniform mat4 u_world;
+const DEFAULT_FILE = "soldier.mdl";
+const RENDER_RESOLUTION = 1000;
+const RENDER_FOV = 1.2;
 
-attribute vec4 a_position;
-attribute vec3 a_delta;
-attribute vec3 a_deltaNormal;
+enum OptionsRenderCamera {
+  Spin = "Spin",
+  Fixed = "Fixed",
+}
 
-attribute vec3 a_normal;
-attribute vec2 a_uv;
+enum OptionsRenderShading {
+  Solid = "Solid",
+  Normals = "Normals",
+  Texture = "Texture",
+  Lighting = "Lighting",
+}
 
-varying vec4 v_position;
-varying vec3 v_normal;
-varying vec2 v_uv;
+enum OptionsRenderMode {
+  Triangles = "Triangles",
+  Points = "Points",
+}
 
-void main() {
-  
-  v_position = u_world * (a_position + vec4(a_delta.x, a_delta.y, a_delta.z, 0.0) * u_frac);
-  v_normal = a_normal  + (a_deltaNormal * u_frac); // TODO :
-  v_uv = a_uv;
-  gl_PointSize = 4.0;
-  gl_Position = u_viewProjection * v_position;
-}`;
-
-const SHADER_FS = `
-#version 100
-precision mediump float;
-
-uniform sampler2D u_texture;
-uniform int u_shading;
-
-varying vec4 v_position;
-varying vec3 v_normal;
-varying vec2 v_uv;
-
-void main() {
-  
-  vec3 l_pos = vec3(150.0, 200.0, 0.0);
-  vec3 l_dir = l_pos - v_position.xyz;
-  vec3 l_nor = normalize(l_dir);
-  float l_amount = dot(l_nor, normalize(v_normal)) * 0.5 + 0.5;
-
-  vec4 l_color = vec4(l_amount, l_amount, l_amount, 1.0);
-
-  if(u_shading == 0) {
-
-    gl_FragColor = texture2D(u_texture, v_uv) * l_color;
-  }
-  else if(u_shading == 1) {
-
-    gl_FragColor = texture2D(u_texture, v_uv);
-  }
-  else {
-    gl_FragColor = vec4(v_normal * 0.5 + 0.5, 1.0);
-  }
-  //gl_FragColor = vec4(v_uv.x,v_uv.y, 0.0, 1.0);
-  // gl_FragColor =   l_color;
-  // gl_FragColor.xyz = v_normal.xyz;
-}`;
+enum OptionsRenderResolution {
+  Half = "half",
+  Full = "full",
+}
 
 (async () => {
+  const handleError = (error: Error) => {
+    UiStatus.Update("status", `⚠️ ${error?.message ?? "Something went wrong"}`);
+  };
+
   try {
-    const mdl = await loadMdl();
-
-    const getGroupValue = (name: string) => {
-      const options = Array.from(
-        document.querySelectorAll(`input[name="${name}"]`)
-      );
-
-      const option = (options.find(
-        (option: HTMLInputElement) => option.checked
-      ) ??
-        options?.[0] ??
-        null) as HTMLInputElement | null;
-
-      return option?.value ?? null;
+    const state = {
+      animationCurrent: null,
+      animationSpeed: 1,
+      animationInterpolate: true,
+      modelSkin: null,
+      modelFile: null,
+      renderCamera: OptionsRenderCamera.Spin,
+      renderShading: OptionsRenderShading.Lighting,
+      renderResolutionFactor: 1,
+      renderMode: OptionsRenderMode.Triangles,
     };
 
-    const bindGroup = (name: string, onChange: (value: string) => void) => {
-      onChange(getGroupValue(name));
+    // Setup UI
 
-      document
-        .querySelectorAll(`input[name="${name}"]`)
-        .forEach((input: HTMLInputElement) => {
-          input.addEventListener("change", () => {
-            onChange(getGroupValue(name));
-          });
-        });
-    };
+    UiDropZone.Bind("file", "Drop MDL file to display", async (event) => {
+      releaseScene?.();
 
-    let selectedAnimationSpeed = 1;
+      const files = Array.from(event.dataTransfer.files);
+      const [file] = files;
 
-    bindGroup("anim-speed", (value) => {
-      selectedAnimationSpeed = Number.parseFloat(value);
+      if (file) {
+        releaseScene = await createScene(file);
+      }
     });
 
-    // const select = document.createElement("select") as HTMLSelectElement;
+    UiRadioGroup.Bind(
+      "animation-speed",
+      [
+        { label: "1/2x", value: "0.5" },
+        { label: "1x", value: "1" },
+        { label: "10x", value: "10" },
+        { label: "20x", value: "20" },
+      ],
+      (item) => {
+        state.animationSpeed = Number.parseFloat(item.value);
+      },
+      `${state.animationSpeed}`
+    );
 
-    const canvas = document.querySelector("canvas") as HTMLCanvasElement;
-    canvas.width = 550;
-    canvas.height = 550;
-    document.body.append(canvas);
+    UiRadioGroup.Bind(
+      "animation-smooth",
+      [
+        { label: "On", value: "true" },
+        { label: "Off", value: "false" },
+      ],
+      (item) => {
+        state.animationInterpolate = item.value === "true";
+      },
+      `${state.animationInterpolate}`
+    );
 
-    const gl = canvas.getContext("webgl");
+    UiRadioGroup.Bind(
+      "render-mode",
+      [
+        { label: "Triangles", value: OptionsRenderMode.Triangles },
+        { label: "Points", value: OptionsRenderMode.Points },
+      ],
+      (item) => {
+        state.renderMode = item.value;
+      },
+      `${state.renderMode}`
+    );
+
+    UiRadioGroup.Bind(
+      "render-shading",
+      [
+        { label: "Solid", value: OptionsRenderShading.Solid },
+        { label: "Normals", value: OptionsRenderShading.Normals },
+        { label: "Texture", value: OptionsRenderShading.Texture },
+        { label: "Lighting", value: OptionsRenderShading.Lighting },
+      ],
+      (item) => {
+        state.renderShading = item.value;
+      },
+      `${state.renderShading}`
+    );
+
+    UiRadioGroup.Bind(
+      "render-camera",
+      [
+        { label: "Fixed", value: OptionsRenderCamera.Fixed },
+        { label: "Spin", value: OptionsRenderCamera.Spin },
+      ],
+      (item) => {
+        state.renderCamera = item.value;
+      },
+      `${state.renderCamera}`
+    );
+
+    UiRadioGroup.Bind(
+      "render-resolution",
+      [
+        { label: "Full", value: OptionsRenderResolution.Full },
+        { label: "Half", value: OptionsRenderResolution.Half },
+      ],
+      (item) => {
+        state.renderResolutionFactor =
+          item.value === OptionsRenderResolution.Full ? 1 : 0.5;
+      },
+      state.renderResolutionFactor === 1
+        ? OptionsRenderResolution.Full
+        : OptionsRenderResolution.Half
+    );
+
+    UiFilePicker.Bind("model-file", async (file) => {
+      releaseScene?.();
+
+      if (file) {
+        releaseScene = await createScene(file);
+      }
+    });
+
+    UiSelect.Bind<MdlAnimation>("animation-current", (animation) => {
+      state.animationCurrent = animation;
+    });
+
+    UiSelect.Bind<number>("model-skin", (modelSkin) => {
+      state.modelSkin = modelSkin;
+    });
+
+    const uiCanvas = UiCanvas.Bind("canvas");
+    uiCanvas.setDimensions(RENDER_RESOLUTION, RENDER_RESOLUTION);
+
+    const gl = uiCanvas.getWebGlContext();
 
     gl.enable(gl.DEPTH_TEST);
 
-    const fov = 1.4; // 30 * Math.PI / 180;
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.1;
-    const zFar = 1000;
+    const createScene = async (file: File) => {
+      let normalBuffer: WebGLBuffer = null;
+      let positionBuffer: WebGLBuffer = null;
+      let deltaBuffer: WebGLBuffer = null;
+      let uvsBuffer: WebGLBuffer = null;
+      let textures: WebGLTexture[] = [];
+      let program: WebGLProgram = null;
 
-    let projection = m4.perspective([], fov, aspect, zNear, zFar);
+      let time = 0;
+      let duration = 0;
+      let rendering = true;
 
-    const onUpdateProjection = () => {
-      const newAspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-      projection = m4.perspective([], fov, newAspect, zNear, zFar);
-    };
-    window.addEventListener("resize", onUpdateProjection);
+      try {
+        UiStatus.Update("status", `Loading..`);
 
-    const program = await createProgram(gl, SHADER_VS, SHADER_FS);
-    const texture = createTexture(
-      gl,
-      mdl.meta.skinWidth,
-      mdl.meta.skinHeight,
-      mdl.skins[0]
-    );
-    const normalBuffer = createAttributeBuffer(gl, mdl.normals);
-    const positionBuffer = createAttributeBuffer(gl, mdl.vertices);
+        const arrayBuffer = await file.arrayBuffer();
+        const arrayReader = new ArrayReader(arrayBuffer);
 
-    const deltas = new Float32Array(mdl.vertices.length);
+        const mdl = await loadMdl(arrayReader);
+        textures = mdl.skins.map((skin) =>
+          createTexture(gl, mdl.meta.skinWidth, mdl.meta.skinHeight, skin)
+        );
 
-    const selectedAnimation = mdl.meta.animations["swing"];
+        program = createProgram(gl, SHADER_VS, SHADER_FS);
 
-    for (const [from, to] of Object.values(mdl.meta.animations)) {
-      for (
-        let frame = /*from*/ from;
-        frame < /*to*/ to /*mdl.meta.numFrames*/;
-        frame++
-      ) {
-        const animationNumFrames = from - to;
+        const animations = extractAnimationsFromFrames(mdl.frames);
+        const deltas = buildFrameDeltas(animations, mdl);
+        const selectedAnimation = animations[Object.keys(animations)[0]]; // animations["walk"];
 
-        const nextFrame = ((frame - from + 1) % animationNumFrames) + from;
-        // const nextFrame =
-        //   ((frame + 1) % animationNumFrames) + selectedAnimation[0];
-        // console.log(animationNumFrames, frame, nextFrame, frame + 1);
-        const frameCurrIndex = frame * mdl.meta.numTriangles * 3 * 3;
-        const frameNextIndex = nextFrame * mdl.meta.numTriangles * 3 * 3;
+        normalBuffer = createAttributeBuffer(gl, mdl.normals);
+        positionBuffer = createAttributeBuffer(gl, mdl.positions);
+        deltaBuffer = createAttributeBuffer(gl, deltas);
+        uvsBuffer = createAttributeBuffer(gl, mdl.uvs);
 
-        for (let triangle = 0; triangle < mdl.meta.numTriangles; triangle++) {
-          const idxCurrTri = triangle * 3 * 3;
+        state.modelFile = mdl;
+        state.animationCurrent = selectedAnimation;
 
-          for (let vertex = 0; vertex < 3; vertex++) {
-            for (let i = 0; i < 3; i++) {
-              const idxCurr = frameCurrIndex + idxCurrTri + vertex * 3 + i;
-              const idxNext = frameNextIndex + idxCurrTri + vertex * 3 + i;
+        const render = () => {
+          if (!rendering) {
+            return;
+          }
 
-              deltas[idxCurr] = mdl.vertices[idxNext] - mdl.vertices[idxCurr];
+          const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+          const zNear = 0.1;
+          const zFar = 500;
 
-              // console.log(idxCurr);
+          const projection = m4.perspective(
+            [],
+            RENDER_FOV,
+            aspect,
+            zNear,
+            zFar
+          );
+          const eye = [150, 20, 20];
+          const target = [0, 0, 0];
+          const up = [0, 1, 0];
+
+          const view = m4.lookAt([], eye, target, up);
+          const timeNext = Date.now();
+          const dt = (timeNext - time) / 1000.0;
+          time = timeNext;
+          duration += dt * state.animationSpeed;
+
+          let animTime = 0;
+          if (state.animationCurrent) {
+            const animationNumFrames =
+              state.animationCurrent[1] - state.animationCurrent[0];
+            animTime =
+              state.animationCurrent[0] + (duration % animationNumFrames);
+          }
+
+          const frame = Math.floor(animTime);
+
+          const u_frac = state.animationInterpolate ? animTime - frame : 0;
+          const u_texture = textures[state.modelSkin ?? 0];
+          const u_world =
+            state.renderCamera === OptionsRenderCamera.Fixed
+              ? m4.identity([])
+              : m4.fromYRotation([], duration);
+          const u_viewProjection = m4.multiply([], projection, view);
+          const u_shading =
+            state.renderShading === OptionsRenderShading.Texture
+              ? 2
+              : state.renderShading === OptionsRenderShading.Normals
+              ? 1
+              : state.renderShading === OptionsRenderShading.Solid
+              ? 0
+              : state.renderShading === OptionsRenderShading.Lighting
+              ? 3
+              : 3;
+
+          gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+          gl.clear(gl.COLOR_BUFFER_BIT);
+          gl.clearColor(0.0, 0.0, 0.0, 0.0);
+
+          bindProgram(gl, program, {
+            textures: {
+              u_texture,
+            },
+            matrix4: {
+              u_viewProjection,
+              u_world,
+            },
+            int: {
+              u_shading,
+            },
+            float: {
+              u_frac,
+            },
+          });
+
+          bindBuffer(gl, program, {
+            a_position: {
+              buffer: positionBuffer,
+              size: 3,
+              type: gl.FLOAT,
+            },
+            a_delta: {
+              buffer: deltaBuffer,
+              size: 3,
+              type: gl.FLOAT,
+            },
+            a_deltaNormal: {
+              buffer: normalBuffer,
+              size: 3,
+              type: gl.FLOAT,
+            },
+            a_normal: {
+              buffer: normalBuffer,
+              size: 3,
+              type: gl.FLOAT,
+            },
+            a_uv: {
+              buffer: uvsBuffer,
+              size: 2,
+              type: gl.FLOAT,
+            },
+          });
+
+          switch (state.renderMode) {
+            case OptionsRenderMode.Points: {
+              gl.drawArrays(
+                gl.POINTS,
+                frame * 3 * mdl.meta.numTriangles,
+                3 * mdl.meta.numTriangles
+              );
+              break;
+            }
+            case OptionsRenderMode.Triangles: {
+              gl.drawArrays(
+                gl.TRIANGLES,
+                frame * 3 * mdl.meta.numTriangles,
+                3 * mdl.meta.numTriangles
+              );
+              break;
             }
           }
-        }
-        /*
-      const idxFrameCurr = mdl.meta.numTriangles * frame * 3;
-      const idxFrameNext = mdl.meta.numTriangles * (frame + 1) * 3;
-      
-      for (let v = 0; v < mdl.meta.numVertices * 3; v++) {
-        deltas[idxFrameCurr + v] = mdl.vertices[idxFrameNext + v] - mdl.vertices[idxFrameCurr + v];
+
+          uiCanvas.setDimensions(
+            RENDER_RESOLUTION * state.renderResolutionFactor,
+            RENDER_RESOLUTION * state.renderResolutionFactor
+          );
+
+          requestAnimationFrame(render);
+        };
+
+        time = Date.now();
+
+        requestAnimationFrame(render);
+
+        UiSelect.Update<MdlAnimation>(
+          "animation-current",
+          Object.entries(animations).map(([label, value]) => ({
+            label,
+            value,
+          }))
+        );
+
+        UiSelect.Update(
+          "model-skin",
+          mdl.skins.concat(mdl.skins).map((_, i) => ({
+            label: `${i}`,
+            value: i,
+          }))
+        );
+
+        UiFilePicker.Update("model-file", file);
+
+        UiStatus.Update("status", null);
+      } catch (error) {
+        handleError(error);
+      } finally {
+        return () => {
+          rendering = false;
+
+          deleteBuffer(gl, normalBuffer);
+          deleteBuffer(gl, positionBuffer);
+          deleteBuffer(gl, deltaBuffer);
+          deleteBuffer(gl, uvsBuffer);
+
+          textures.forEach((texture) => deleteTexture(gl, texture));
+
+          deleteProgram(gl, program);
+        };
       }
-      */
-      }
-    }
-
-    const deltaBuffer = createAttributeBuffer(gl, deltas);
-    const uvsBuffer = createAttributeBuffer(gl, mdl.uvs);
-
-    let time = 0;
-
-    const renderFrame = () => {
-      //  const time = 0; //Date.now() * -0.0005;
-      const eye = [150, 100, 100]; // [Math.sin(time) * 200, 0 * Math.cos(time), 200 * Math.cos(time)];
-
-      const target = [0, 0, 0];
-      const up = [0, 1, 0];
-
-      const view = m4.lookAt([], eye, target, up);
-      const world = m4.fromYRotation([], time * 0.01);
-      const viewProjection = m4.multiply([], projection, view);
-
-      time += selectedAnimationSpeed;
-
-      const animationNumFrames = selectedAnimation[1] - selectedAnimation[0];
-      const animTime = selectedAnimation[0] + (time % animationNumFrames);
-
-      let frame = Math.floor(animTime); // % mdl.meta.numFrames;
-      const frac = animTime - frame;
-
-      gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-      gl.clear(gl.COLOR_BUFFER_BIT);
-      gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-      bindProgram(gl, program, {
-        textures: {
-          u_texture: texture,
-        },
-        matrix4: {
-          u_viewProjection: viewProjection,
-          u_world: world,
-        },
-        int: {
-          u_shading: 2,
-        },
-        float: {
-          u_frac: frac,
-        },
-      });
-
-      bindBuffer(gl, program, {
-        a_position: {
-          buffer: positionBuffer,
-          size: 3,
-          type: gl.FLOAT,
-        },
-        a_delta: {
-          buffer: deltaBuffer,
-          size: 3,
-          type: gl.FLOAT,
-        },
-        a_deltaNormal: {
-          buffer: normalBuffer,
-          size: 3,
-          type: gl.FLOAT,
-        },
-        a_normal: {
-          buffer: normalBuffer,
-          size: 3,
-          type: gl.FLOAT,
-        },
-        a_uv: {
-          buffer: uvsBuffer,
-          size: 2,
-          type: gl.FLOAT,
-        },
-      });
-
-      /*
-      gl.drawArrays(gl.POINTS, frame * 3 * mdl.meta.numTriangles, 3 * mdl.meta.numTriangles);
-      */
-      gl.drawArrays(
-        gl.TRIANGLES,
-        frame * 3 * mdl.meta.numTriangles,
-        3 * mdl.meta.numTriangles
-      );
-
-      requestAnimationFrame(renderFrame);
     };
 
-    renderFrame();
-
-    ///
-    /*
-    TODO:!!!
-
-    gl.useProgram(null);
-    gl.deleteBuffer(positionBuffer);
-    gl.deleteProgram(program);
-    */
-  } catch (err) {
-    console.error(err);
+    const file = await fileFromPath(`/${DEFAULT_FILE}`);
+    let releaseScene = await createScene(file);
+  } catch (error) {
+    handleError(error);
   }
 })();
