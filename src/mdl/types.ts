a4eba910c5561099e77abb8e142a7a37b9902c02
Dacre <dacre@mooce.co.nz>
2022-03-19 01:57:35 +1300

    Add MDL file interface

--- src/mdl/types.ts
+++ src/mdl/types.ts
@@ -1,3 +1,18 @@
+export interface MdlFile {
+  positions: Float32Array;
+  uvs: Float32Array;
+  normals: Float32Array;
+  skins: Uint8Array[];
+  frames: MdlFrame[];
+  meta: {
+    skinWidth: number;
+    skinHeight: number;
+    numFrames: number;
+    numTriangles: number;
+    numVertices: number;
+  };
+}
+
 export interface MdlFrame {
   name: string;
   frame: number;
